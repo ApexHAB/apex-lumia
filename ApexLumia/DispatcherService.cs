4566dd4c76360fbd534a663b27b0d8603305e5b8
Daniel Saul <daniel@dansaul.co.uk>
2012-07-05 17:47:17 +0100

    Added Dispatcher Service thing... has to be called regularly to make the sound work for some reason.

---
+++ ApexLumia/DispatcherService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Threading;
+using Microsoft.Xna.Framework;
+
+namespace ApexLumia
+{
+    public class XNAFrameworkDispatcherService : IApplicationService
+    {
+        private DispatcherTimer frameworkDispatcherTimer;
+
+        public XNAFrameworkDispatcherService()
+        {
+            this.frameworkDispatcherTimer = new DispatcherTimer();
+            this.frameworkDispatcherTimer.Interval = TimeSpan.FromTicks(333333);
+            this.frameworkDispatcherTimer.Tick += frameworkDispatcherTimer_Tick;
+            FrameworkDispatcher.Update();
+        }
+
+        void frameworkDispatcherTimer_Tick(object sender, EventArgs e) { FrameworkDispatcher.Update(); }
+
+        void IApplicationService.StartService(ApplicationServiceContext context) { this.frameworkDispatcherTimer.Start(); }
+
+        void IApplicationService.StopService() { this.frameworkDispatcherTimer.Stop(); }
+    }
+}
