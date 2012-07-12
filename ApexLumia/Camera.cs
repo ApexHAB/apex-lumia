5ba4d8fe33b56cbbb8429ba5996563890d67c4c0
Daniel Saul <daniel@dansaul.co.uk>
2012-07-12 15:11:23 +0100

    Added further references.

--- ApexLumia/Camera.cs
+++ ApexLumia/Camera.cs
@@ -2,6 +2,11 @@
 using System.Net;
 using System.Windows;
 using Microsoft.Xna.Framework;
+using Microsoft.Phone.Tasks;
+using System.Windows.Media.Imaging;
+using System.IO;
+using System.IO.IsolatedStorage;
+using Microsoft.Xna.Framework.Media;
 
 namespace ApexLumia
 {
