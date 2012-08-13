3b1b45af3c7cc8efaac2fa06e235f9c30f5beb65
Daniel Saul <daniel@dansaul.co.uk>
2012-08-13 10:06:23 +0100

    Added Flightloop & Sentence classes

---
+++ ApexLumia/FlightLoop.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Threading;
+
+namespace ApexLumia
+{
+    public class FlightLoop
+    {
+
+        bool _isRunning = false;
+        public bool isRunning { get { return _isRunning; } }
+
+        public void start()
+        {
+            Thread loopThread = new Thread(new ThreadStart(this.loop));
+            loopThread.IsBackground = true;
+            loopThread.Name = "Flight Loop";
+
+            _isRunning = true;
+            loopThread.Start();
+        }
+
+        public void stop()
+        {
+            _isRunning = false;
+        }
+
+        private void loop()
+        {
+            while (_isRunning == true)
+            {
+
+                // Main Loop Here. Yay. Whoop. No.
+
+                // Collect Data into Sentence object
+
+                // Construct sentence, checksum etc.
+
+                // Log sentence & data
+
+                // Transmit sentence
+
+                // Upload to habitat, if internet
+
+                // Twitter, if internet
+
+                // Take photo - or video?
+
+                // Upload photos to SkyDrive
+
+                // Probably wait a bit
+
+
+            }
+        }
+
+    }
+}
